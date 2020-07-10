2a2d039e9ee6e27083105d3a7d8e7051128f0f7f
Vins <sharmavins23@gmail.com>
2020-07-10 12:01:22 -0400

    🎉 initial upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Jasmine
+
+![img](https://images-na.ssl-images-amazon.com/images/I/61JIawAiH0L._AC_SY355_.jpg)
---
+++ jasmine.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+import commando from "discord.js-commando";
+import path from "path";
+
+// * Client initialization
+const client = new commando.Client({
+    owner: process.env.OWNER,
+    commandPrefix: process.env.PREFIX,
+    nonCommandEditable: true,
+});
+
+// * Event handlers
+// Startup
+client.on("ready", () => {
+    client.user
+        .setStatus("online")
+        .setActivity("with the linter", { type: "PLAYING" })
+        .then((presence) =>
+            console.log(
+                `Jasmine is ${presence.activities[0].name} on ${client.guilds.size} servers.`
+            )
+        )
+        .catch(console.error);
+});
+
+// Guild events
+client.on("guildCreate", (guild) =>
+    console.log(`Jasmine has joined ${guild.name}. ID: ${guild.id}`)
+);
+client.on("guildDelete", (guild) =>
+    console.log(`Jasmine has left ${guild.name}. ID: ${guild.id}`)
+);
+
+// Error handling
+client.on("error", (e) => {
+    client.user.setStatus("dnd");
+    console.error(e);
+});
+client.on("warn", (e) => {
+    client.user.setStatus("idle");
+    console.warn(e);
+});
+
+// * Command registry
+client.registry
+    .registerDefaultTypes()
+    .registerGroups([
+        ["basic", "Basic commands"],
+        ["owner", "Owner-locked commands"],
+    ])
+    .registerDefaultGroups()
+    .registerDefaultCommands()
+    .registerCommandsIn(path.join(__dirname, "commands"));
+
+// * Magic!
+client.login(process.env.CLIENT_TOKEN);
